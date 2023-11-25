63ee2492cb837bc0f7a272b22efae36fdc021309
lukman-ss <64991223+lukman-ss@users.noreply.github.com>
2023-11-25 17:01:30 +0700

    add course controller

--- app/Models/Course/CourseModel.php
+++ app/Models/Course/CourseModel.php
@@ -2,8 +2,9 @@
 
 namespace App\Models\Course;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CourseModel extends Model
 {
@@ -17,4 +18,15 @@ class CourseModel extends Model
         'status',
         'created_by',
     ];
+    /**
+     * image
+     *
+     * @return Attribute
+     */
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => asset('/storage/posts/' . $image),
+        );
+    }
 }
