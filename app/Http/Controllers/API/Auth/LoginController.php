a0fe733a4f062a6ffb56ef3bfa1800643a4575cf
lukman-ss <64991223+lukman-ss@users.noreply.github.com>
2023-11-25 17:44:07 +0700

    add cauith resource and auth controller

---
+++ app/Http/Controllers/API/Auth/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
