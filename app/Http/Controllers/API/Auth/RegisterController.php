4bc6f3102c5dcaaa8027cb883bad2b7a00909fd8
lukman-ss <64991223+lukman-ss@users.noreply.github.com>
2023-11-25 18:30:22 +0700

    update auth register

--- app/Http/Controllers/API/Auth/RegisterController.php
+++ app/Http/Controllers/API/Auth/RegisterController.php
@@ -5,6 +5,13 @@ namespace App\Http\Controllers\API\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Resources\Auth\AuthResource;
+
+use Illuminate\Support\Facades\Validator;
+
 class RegisterController extends Controller
 {
     /**
@@ -12,6 +19,33 @@ class RegisterController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        // Validate the incoming request data
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        // Check if validation fails
+        if ($validator->fails()) {
+            // Return validation errors in a structured format
+            return new AuthResource(false, 400, 'Validation failed', $validator->errors());
+        }
+
+        try {
+            // Attempt to create a new user
+            $user = User::create([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => Hash::make($request->input('password')),
+            ]);
+    
+            // Return a successful response
+            return new AuthResource(true, 200, 'Auth Login', $user);
+    
+        } catch (\Exception $e) {
+            // Handle any other unexpected errors
+            return new AuthResource(false, 500, 'Error creating user', null, $e->getMessage());
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::prefix('auth')->group(function () {
     Route::post('/login', [App\Http\Controllers\API\Auth\LoginController::class, '__invoke']);
+    Route::post('/register', [App\Http\Controllers\API\Auth\RegisterController::class, '__invoke']);
     // Route::post('/login', [AuthController::class, 'login']);
     // Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
     // Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'user']);
