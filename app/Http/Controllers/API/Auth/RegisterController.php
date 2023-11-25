717b1cbd111c6263fdf2c153b949a8360e5517f8
lukman-ss <64991223+lukman-ss@users.noreply.github.com>
2023-11-25 19:33:53 +0700

    update auth register

--- app/Http/Controllers/API/Auth/RegisterController.php
+++ app/Http/Controllers/API/Auth/RegisterController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\API\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Resources\Auth\AuthResource;
