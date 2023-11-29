8d56688c2963513a105e3fdaa2cd6d7b11f8be2f
lukman-ss <64991223+lukman-ss@users.noreply.github.com>
2023-11-29 13:03:56 +0700

    added crouse secret key model and migration

--- app/Models/Course/CourseScheduledModel.php
+++ app/Models/Course/CourseScheduledModel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\CourseScheduled;
+namespace App\Models\Course;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
---
+++ app/Models/Course/CourseSecretKeyModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Course;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CourseSecretKeyModel extends Model
+{
+    use HasFactory;
+}
